6434c1924350246f279bc5a7b5e862b37fbc5d2b
Muhammed Talha Karagul <talha.karagul@is.net.tr>
2024-09-05 18:23:46 +0300

    get,put,post methods are done

--- BookStoreWebapi/Controllers/BookController.cs
+++ BookStoreWebapi/Controllers/BookController.cs
@@ -1,11 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BookStoreWebapi{
+namespace BookStoreWebapi.Controllers 
+{
 
     [ApiController]
-    [Route("[contoller]s")]
+    [Route("[controller]s")]
     public class BookController : ControllerBase
     {
+        private static List<Book> BookList = new List<Book> {
+          new Book {
+            Id = 1,
+            Title="Lean Startup",
+            GenreId=1, //Personal Growth
+            PageCount = 200,
+            PublishDate = new DateTime(2001,08,12)
+          } ,
+            new Book {
+            Id = 2,
+            Title="Herhald",
+            GenreId=2, //Science Fiction
+            PageCount = 250,
+            PublishDate = new DateTime(2010,05,23)
+          } ,  
+          new Book {
+            Id = 3,
+            Title="Dune",
+            GenreId=2, //Science Fiction
+            PageCount = 540,
+            PublishDate = new DateTime(2001,12,21)
+          }  
+        };
+
+        [HttpGet]
+        public List<Book> GetBooks(){
+            var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;
+        }
         
+        [HttpGet("{id}")]
+        public Book GetById(int id){
+            // var book = BookList.Where(x=> x.Id == id).FirstOrDefault();
+            // var book = BookList.Where(x=> x.Id == id);
+            var book = BookList.Where(x=> x.Id == id).SingleOrDefault();
+            return book;
+        }
+
+        // [HttpGet] // aynı anda 2 tane http get i yazamıyorsun eğer yazarsan conflict yersin haberin olsun.
+        // public Book GetByIdQuerry([FromQuery]int  id){
+        //     // var book = BookList.Where(x=> x.Id == id).FirstOrDefault();
+        //     // var book = BookList.Where(x=> x.Id == id);
+        //     var book = BookList.Where(x=> x.Id == id).SingleOrDefault();
+        //     return book;
+        // }
+
+        [HttpPost]
+        //complex tiplerde yani classlar objetlerde from body kullanama gerek yok ama basit tiplerde
+        //yani int string bool gibi ifadelerde from body kullanmak zorundasın 
+        public IActionResult AddBook([FromBody]Book newBook){
+
+            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+
+            if(book is not null)
+                return BadRequest();
+            
+            BookList.Add(newBook);
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody]Book updatedBook){
+
+            var book = BookList.SingleOrDefault(x=> x.Id == id);
+            if (book is null)
+                return BadRequest();
+            
+            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+            book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
+            book.Title=updatedBook.Title != default ? updatedBook.Title : book.Title;
+            book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+
+            return Ok();
+
+        }
+
     }
 }
